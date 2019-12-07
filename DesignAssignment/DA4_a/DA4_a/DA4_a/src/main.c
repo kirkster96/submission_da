ba7cc9b036f167efff02339746f87fd9f141bc77
Cameron Kirk <kirkster96@gmail.com>
2019-12-07 14:24:32 -0800

    Design Assignment 4A
    Signed-off-by: Cameron Kirk <kirkster96@gmail.com>

---
+++ DesignAssignment/DA4_a/DA4_a/DA4_a/src/main.c
@@ -0,0 +1,144 @@
+#define F_CPU 16000000UL
+#define ENABLE 3
+#define MTR_1 1
+#define MTR_2 2
+#define SW (PINC&(1<<1))
+#define BAUD_RATE 9600
+#define BAUD_PRESCALE (((F_CPU / (BAUD_RATE * 16UL))) - 1)
+
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+//UART for debug purposes on terminal
+void usart_init (void);
+void usart_send (char ch);
+void UART_sendString(const char *str);
+
+
+int main (void)
+{
+	usart_init();
+	//configure the PWM
+	DDRD |= (1<<DDD6); //PD6 is now a output
+	OCR0A = 127;//set PWM for 50% dutycycle;
+	
+	TCCR0A |= (1<<COM0A1)|(1<<WGM01) |(1<<WGM00);
+	TCCR0B |=(1<<CS01);
+	
+	
+	
+	
+	//initialize the ADC
+	ADMUX = (0<<REFS1)|	//Reference selection bits
+	(1<<REFS0)|			//AVcc - external cap at AREF
+	(0<<ADLAR)|			//Left adjust most significant bit
+	(0<<MUX2)|			//Analog channel selection bits
+	(0<<MUX1)|			//ADC0 (PC0 PIN23)
+	(0<<MUX0);
+	ADCSRA = (1<<ADEN)|	//	Enable ADC
+	(0<<ADSC)|			//ADC start conversion
+	(0<<ADATE)|			//ADC Autotrigger enable
+	(0<<ADIF)|			//ADC Interrupt Flag
+	(0<<ADIE)|			//ADC Interrupt Enable
+	(1<<ADPS2)|			//ADC prescaler bits
+	(0<<ADPS1)|
+	(1<<ADPS0);
+	
+	DDRB |= (1<<DDB1)|(1<<DDB2)|(1<<DDB3);
+	//PB1,PB2, and PB3 is now an output
+	
+	PORTC |= (1<<0)|(1<<1)|(1<<2)|(1<<3);
+	//Enable PC1 as input
+	
+	PORTB &= ~(1<<ENABLE);
+	PORTB &= ~(1<<MTR_1);
+	PORTB &= ~(1<<MTR_2);
+	
+	PCMSK1 |= (1<<PCINT9)|	//enable PC1 as External Interrupt
+	(1<<PCINT10)|				//enable PC2 as External Interrupt
+	(1<<PCINT11);			//enable PC3 as External Interrupt
+
+	//enable interrupts
+	PCICR |= (1<<PCIE1);
+	sei();
+	
+	//configure to trigger on falling edge
+	EICRA |= (1<<ISC11);
+	EICRA &= ~(1<<ISC10);
+
+	
+	PORTB |= (1<<ENABLE);
+
+	volatile unsigned char TEMP [4];
+	
+	while(1){
+		ADCSRA |= (1<<ADSC);			//start conversion
+		while((ADCSRA&(1<<ADIF))==0);	//wait for conversion to complete
+		
+		ADCSRA |= (1<<ADIF);
+		
+		//output adc results to terminal
+		
+		
+		int c = ADCL;
+		c = c | (ADCH<<8);
+		c = (c/1024.0)*5000/10;
+		
+		itoa(c,TEMP,10);	
+		
+		UART_sendString(TEMP);
+		UART_sendString("\r\n");
+
+		
+		//c<11 -> Duty Cycle  0%
+		//c>490 -> Duty Cycle 95%
+		OCR0A = c/2;
+
+
+		_delay_ms(20);
+		PORTB &= ~(1<<MTR_2);
+		PORTB |= (1<<MTR_1);
+
+	}
+}
+
+ISR (PCINT1_vect){
+
+		PORTB ^= (1<<ENABLE);
+		_delay_ms(200);
+
+
+
+
+}
+void usart_init(void){
+	//turn on Rx, TX
+	UCSR0B |= (1<<RXEN0)|(1<<TXEN0);
+	//8bit char sizes
+	UCSR0C |= (1<<UCSZ01)|(1<<UCSZ00);
+	//set baud rate
+	UBRR0H = (BAUD_PRESCALE >> 8);
+	UBRR0L = BAUD_PRESCALE;
+	//Enable the USART Receive interrupt
+	UCSR0B |= (1<<RXCIE0);
+	
+	//globally enable interrupts
+	//sei();
+	
+}
+
+void usart_send(char ch){
+	while(! (UCSR0A & (1<<UDRE0)));	//wait until UDR0 is empty
+	UDR0 = ch;		//transmit ch
+}
+
+void UART_sendString(const char *str){
+	
+	while(*str){
+		usart_send(*str++);
+	}
+}
